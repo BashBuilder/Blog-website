eeeb9567847bb4fe6c104a20a89c2c64269587f8
unknown <iamadelowotan@gmail.com>
2025-07-07 10:23:18 +0100

    update: bug fixed

--- src/app/(frontend)/next/seed/route.ts
+++ src/app/(frontend)/next/seed/route.ts
@@ -19,6 +19,7 @@ export async function POST(): Promise<Response> {
   try {
     // Create a Payload request object to pass to the Local API for transactions
     // At this point you should pass in a user, locale, and any other context you need for the Local API
+    //@ts-ignore
     const payloadReq = await createLocalReq({ user }, payload)
 
     await seed({ payload, req: payloadReq })

26377a627abd00a35be6d1928797acd48a6c0688
unknown <iamadelowotan@gmail.com>
2025-07-07 13:38:41 +0100

    update: picture caching added

--- next.config.js
+++ next.config.js
@@ -4,13 +4,16 @@ import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'https://qorepay-blog.vercel.app'
+  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[
+        'https://qorepay-blog.vercel.app',
+        NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */,
+      ].map((item) => {
         const url = new URL(item)
 
         return {
